51cbf45ce29eaeeb19633d3dd4a603b2ada8125d
Guillermo Mainini <guimainini@gmail.com>
2021-02-15 09:35:47 -0300

    mostrar mensaje de error, comando viewChild

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
+  @ViewChild('miForm') miForm!: NgForm
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  save(){
+    console.log( this.miForm )
+  }
+  
+  validName(): boolean {
+    return this.miForm?.controls.produc?.invalid && 
+                this.miForm?.controls.produc?.touched
+  }
+
 
 }
