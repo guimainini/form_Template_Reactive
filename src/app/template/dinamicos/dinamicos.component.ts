2fef8ca06a6677a7f713ae771d3ae8bc1f2ffe27
Guillermo Mainini <guimainini@gmail.com>
2021-02-17 12:02:20 -0300

    termine toda la parte de template dimanico, input y arreglos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,6 +1,16 @@
 import { ClassGetter } from '@angular/compiler/src/output/output_ast';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
+
+
+interface Person {
+  name: string;
+  favorite: Favorite[];
+}
+
+interface Favorite {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-dinamicos',
@@ -8,20 +18,38 @@ import { NgForm } from '@angular/forms';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent   {
+
+  nuevoJuego: string='';
 
 
- 
+  person: Person = {
+    name: 'Guillermo',
+    favorite:[
+      { id: 1, name: 'Metal Gear'},
+      { id: 2, name: 'DeathStraning'}
 
-  constructor() { }
-
-  ngOnInit(): void {
+    ]
   }
-
+  
   save(){
     console.log('Form posteado');
-    
+  }
+ 
+  eliminar( index:number ){
+    this.person.favorite.splice(index, 1);
   }
 
+  agregarJuego(){
+    const newFavorite: Favorite = {
+        id: this.person.favorite.length + 1,
+        name: this.nuevoJuego
 
+    }
+
+    this.person.favorite.push({...newFavorite});
+    this.nuevoJuego= '';
+  }
 }
+
+
