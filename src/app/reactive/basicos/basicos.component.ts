5264a60f953c2c1bd8984e0c0fe155a767e8b89d
Guillermo Mainini <guimainini@gmail.com>
2021-02-18 15:16:56 -0300

    terminado toda la parte de basicos reactive(todas la validaciones del lado de ts)

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,20 +1,55 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators, Validator } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html'
 })
-export class BasicosComponent {
+export class BasicosComponent implements OnInit{
 
-  miForm: FormGroup = new FormGroup({
-    'name': new FormControl('RTX 4080ti')
+  // miForm: FormGroup = new FormGroup({
+  //   'name'   : new FormControl('RTX 4080ti'),
+  //   'price'  : new FormControl(1500),
+  //   'stocks' : new FormControl(5)
+  // })
+
+  miForm: FormGroup = this.fb.group({
+                          //validadores sincronos//validadores asincronos
+    name    : [ , [Validators.required, Validators.minLength(3)], ],
+    price   : [ , [Validators.min(0),Validators.required], ],
+    stocks  : [ , [Validators.min(0),Validators.required], ],
   })
 
 
+  constructor( private fb: FormBuilder ) { }
 
-  constructor() { }
+  ngOnInit(){
+    this.miForm.setValue({
+      name   : ' RTX 4080ti',
+      price  : 1600,
+      stocks : 10
+    })
+  }
 
+
+  validField( campo: string ) {
+    return this.miForm.controls[campo].errors && this.miForm.controls[campo].touched
+  }
   
+  save(){
+
+    if(this.miForm.invalid){
+
+      this.miForm.markAllAsTouched();
+
+
+      return;
+    }
+
+    console.log(this.miForm.value);
+    this.miForm.reset();
+    
+
+  }
 
 }
