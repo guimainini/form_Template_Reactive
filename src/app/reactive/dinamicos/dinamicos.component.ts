ff191cf8bc57d123044a16066b0b2b7323003de1
Guillermo Mainini <guimainini@gmail.com>
2021-02-19 08:40:18 -0300

    terminado la parte de reactive dinamicos

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, Validator, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +7,65 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent   {
 
-  constructor() { }
+  miForm: FormGroup = this.fb.group({
+                      //validadores sincronos//validadores asincronos
+      name     : [ '', [Validators.required, Validators.minLength(3)], ],
+      favority : this.fb.array( [
+        ['Metal Gear', Validators.required],
+        ['Death Stranding', Validators.required]
+      ], Validators.required )
+  });
 
-  ngOnInit(): void {
+  newFavority: FormControl = this.fb.control('', Validators.required);
+
+
+
+  constructor( private fb: FormBuilder ) { }
+
+
+  get favoritosArr() {
+    return this.miForm.get('favority') as FormArray;
   }
+
+  validField( campo: string ) {
+    return this.miForm.controls[campo].errors && this.miForm.controls[campo].touched
+  }
+  
+  save(){
+
+    if(this.miForm.invalid){
+      this.miForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.miForm.value);
+  }  
+
+
+
+
+  addFavority(){
+    if( this.newFavority.invalid ){
+      return;
+    }
+
+    //this.favoritosArr.push( new FormControl(this.newFavority.value, Validators.required));
+    this.favoritosArr.push( this.fb.control(this.newFavority.value, Validators.required) );
+    this.newFavority.reset();
+
+  }
+
+  // eliminar( index:number ){
+  //   this.person.favorite.splice(index, 1);
+  // }
+
+  delete( index:number ){
+    //console.log(index);
+    this.favoritosArr.removeAt(index);
+    
+    
+  }
+
 
 }
