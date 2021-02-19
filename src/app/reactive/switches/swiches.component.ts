fdbf5e46cfd0622df2e8e3b217f4550bdc7594f0
Guillermo Mainini <guimainini@gmail.com>
2021-02-19 16:23:23 -0300

    terminado la parte de reactive swiches

--- src/app/reactive/switches/swiches.component.ts
+++ src/app/reactive/switches/swiches.component.ts
@@ -1,4 +1,6 @@
+import { ClassGetter } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, Validator } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -6,11 +8,53 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SwichesComponent implements OnInit {
+export class SwichesComponent implements OnInit  {
 
-  constructor() { }
+  miForm:FormGroup= this.fb.group({
+    gender: [ 'M', Validators.required],
+    notify: [ true, Validators.required],
+    condicion: [false, Validators.requiredTrue]
 
-  ngOnInit(): void {
+  });
+
+  person= {
+    gender : 'F',
+    notify : true
+  } 
+
+  constructor( private fb: FormBuilder ) { }
+
+  ngOnInit(){
+    this.miForm.reset({
+      ...this.person,
+    condicion: false
+  });
+
+  // this.miForm.get('condicion')?.valueChanges.subscribe( newValue => {
+  //   console.log( newValue );
+  // } )
+    
+  
+  this.miForm.valueChanges.subscribe( form => {
+    delete form.condicion;
+    this.person = form;
+    
+  } )
+
+    
+
+    
+
+
   }
 
+  save(){
+    const formValue = {...this.miForm.value}
+      //delete formValue.condicion;
+
+      console.log(formValue);
+      
+      this.person = formValue;
+
+  }
 }
